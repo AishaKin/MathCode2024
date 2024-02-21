9900b67ef42dfb838ec4e2429b336ddd5a07e797
AishaKin <aisha2004@inbox.ru>
2024-02-21 20:36:12 +0300

    add vegenere

--- homework02/rsa.py
+++ homework02/rsa.py
@@ -6,7 +6,7 @@ def is_prime(n: int) -> bool:
     if n < 2:
         return False
     i = 2
-    while i*i <= n:
+    while i * i <= n:
         if n % i == 0:
             return False
         i += 1
--- homework02/vigenere.py
+++ homework02/vigenere.py
@@ -1,30 +1,40 @@
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
-
-    >>> encrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> encrypt_vigenere("python", "a")
-    'python'
-    >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
-    'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+
+    for i in range(len(plaintext)):
+        char = plaintext[i]
+        if char.isalpha():
+            shift = ord(keyword[i % len(keyword)]) - ord('A')
+            if char.isupper():
+                ciphertext += chr((ord(char) + shift - ord('A')) % 26 + ord('A'))
+            else:
+                ciphertext += chr((ord(char) + shift - ord('a')) % 26 + ord('a'))
+        else:
+            ciphertext += char
+
     return ciphertext
 
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
     Decrypts a ciphertext using a Vigenere cipher.
-
-    >>> decrypt_vigenere("PYTHON", "A")
-    'PYTHON'
-    >>> decrypt_vigenere("python", "a")
-    'python'
-    >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
-    'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword.upper()
+
+    for i in range(len(ciphertext)):
+        char = ciphertext[i]
+        if char.isalpha():
+            shift = ord(keyword[i % len(keyword)]) - ord('A')
+            if char.isupper():
+                plaintext += chr((ord(char) - shift - ord('A')) % 26 + ord('A'))
+            else:
+                plaintext += chr((ord(char) - shift - ord('a')) % 26 + ord('a'))
+        else:
+            plaintext += char
+
     return plaintext
