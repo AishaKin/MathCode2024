14978252ddaa6af1bf1a2e4f7f2ae3fa43a1d73f
AishaKin <aisha2004@inbox.ru>
2024-02-21 20:05:33 +0300

    add rsa and caesar move

--- homework02/rsa.py
+++ homework02/rsa.py
@@ -3,16 +3,6 @@ import typing as tp
 
 
 def is_prime(n: int) -> bool:
-    """
-    Tests to see if a number is prime.
-
-    >>> is_prime(2)
-    True
-    >>> is_prime(11)
-    True
-    >>> is_prime(8)
-    False
-    """
     if n < 2:
         return False
     i = 2
@@ -23,30 +13,26 @@ def is_prime(n: int) -> bool:
     return True
 
 
-
 def gcd(a: int, b: int) -> int:
-    """
-    Euclid's algorithm for determining the greatest common divisor.
-
-    >>> gcd(12, 15)
-    3
-    >>> gcd(3, 7)
-    1
-    """
-    # PUT YOUR CODE HERE
-    pass
+    while a != 0 and b != 0:
+        if a >= b:
+            a %= b
+        else:
+            b %= a
+    return a or b
 
 
 def multiplicative_inverse(e: int, phi: int) -> int:
-    """
-    Euclid's extended algorithm for finding the multiplicative
-    inverse of two numbers.
+    if e == 0:
+        return 0
 
-    >>> multiplicative_inverse(7, 40)
-    23
-    """
-    # PUT YOUR CODE HERE
-    pass
+    phi0, x0, x1 = phi, 0, 1
+    while e > 1:
+        q = e // phi
+        x0, x1 = x1 - q * x0, x0
+        e, phi = phi, e % phi
+
+    return x1 % phi0 if x1 >= 0 else phi0 + x1
 
 
 def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[int, int]]:
@@ -56,10 +42,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
