70711f4e118d0cd5a2c3185e868631ee96b1bdf9
AishaKin <aisha2004@inbox.ru>
2024-02-21 20:54:54 +0300

    changes caesar

--- homework02/caesar.py
+++ homework02/caesar.py
@@ -1,48 +1,15 @@
 import typing as tp
 
-# str.maketrans('ar', 'by') #сам сменяет символы на код и создает словарь
-# 'htllo'.translate({})
-#
-# 'arroz'.translate({97:98, 114:121})
-# ord('a') #получить код символа
-#
-# abc = 'abcdefghij...'
-# ces = 'defghij...' #создаем словарь и словарь шифра и передаем в макетранс далее в транслейт
-# АВС = 'ABCDEF...'
-#
-# d1 = {1:2, 2:3}
-# d2 = {2:3}
-# d1.update(d2)
-# d_all ={**d1, ***d2}
-#
-# d_end = [shift:] + [:shift]
-#
-# print(*a) #чтобы каждый элемент словарепй, списков отдельно выводился
-
-abc = 'abcdefghijklmnopqrstuvwxyz'#string.ascii_letters
-
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ciphertext = ""
 
-    for i in range(len(plaintext)):
-        symbol = ord(plaintext[i])
-        if (symbol > 64) and (symbol < 91):
-            if (symbol + shift) > 90:
-                ciphertext += chr(symbol - 26 + shift)
-
-                continue
-
-            ciphertext += chr(symbol + shift)
-        elif (symbol > 96) and (symbol < 122):
-            if (symbol + shift) > 121:
-                ciphertext += chr(symbol - 26 + shift)
-
-                continue
-
-            ciphertext += chr(symbol + shift)
+    for char in plaintext:
+        if char.isalpha():
+            base = ord('a') if char.islower() else ord('A')
+            ciphertext += chr((ord(char) - base + shift) % 26 + base)
         else:
-            ciphertext += plaintext[i]
+            ciphertext += char
 
     return ciphertext
 
@@ -50,30 +17,37 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
 
-    for i in range(len(ciphertext)):
-        symbol = ord(ciphertext[i])
-        if (symbol > 64) and (symbol < 91):
-            if (symbol - shift) < 65:
-                plaintext += chr(symbol + 26 - shift)
-                continue
-
-            plaintext += chr(symbol - shift)
-        elif (symbol > 96) and (symbol < 122):
-            if (symbol - shift) < 97:
-                plaintext += chr(symbol + 26 - shift)
-                continue
-
-            plaintext += chr(symbol - shift)
+    for char in ciphertext:
+        if char.isalpha():
+            base = ord('a') if char.islower() else ord('A')
+            plaintext += chr((ord(char) - base - shift) % 26 + base)
         else:
-            plaintext += ciphertext[i]
+            plaintext += char
 
     return plaintext
 
 
 def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
-    """
-    Brute force breaking a Caesar cipher.
-    """
+    def score(decrypted_text: str) -> int:
+        words = decrypted_text.split()
+        return sum(1 for word in words if word.lower() in dictionary)
+
     best_shift = 0
-    # PUT YOUR CODE HERE
+    max_score = 0
+
+    for shift in range(26):
+        decrypted_text = ""
+        for char in ciphertext:
+            if char.isalpha():
+                decrypted_char = chr((ord(char.lower()) - ord('a') - shift) % 26 + ord('a'))
+                decrypted_char = decrypted_char.upper() if char.isupper() else decrypted_char
+                decrypted_text += decrypted_char
+            else:
+                decrypted_text += char
+
+        current_score = score(decrypted_text)
+        if current_score > max_score:
+            max_score = current_score
+            best_shift = shift
+
     return best_shift
