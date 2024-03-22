b02e05991304d1716cd7091fa46b5077fe731c7e
AishaKin <aisha2004@inbox.ru>
2024-03-22 14:15:05 +0300

    lab3

--- homework03/server.py
+++ homework03/server.py
@@ -1,61 +1,76 @@
+# save this as app.py
 import time
+import flask
+from flask import Flask, abort
 from pydantic import BaseModel
-from fastapi import FastAPI
 
-app = FastAPI()
-
-class Message(BaseModel):
-    name: str
-    text: str
-
+app = Flask(__name__)
 db = []
-for i in range(3):
-    db.append({
-        'name': 'Anton',
-        'time': 12343,
-        'text': 'text01923097'
-    })
 
-@app.get("/")
+
+@app.route("/")
 def hello():
     return "Hello, World!"
 
-@app.post("/send")
-def send_message(message: Message):
-    '''
-    функция для отправки нового сообщения пользователем
-    :return:
-    '''
-    # Добавить сообщение в базу данных db
-    data = message.dict()
-    data['time'] = time.time()
-    db.append(data)
+class data1(BaseModel):
+    name: str
+    text: str
+
+
+@app.route("/send", methods=['POST'])
+def send_message():
+    data = flask.request
+    n = data.data
+
+    try:
+        n = data1.model_validate_json(n)
+        if n.name == '' or n.text == '':
+            return abort(400)
+    except ValueError:
+        return abort(400)
+
+    data = flask.request.json
+    text = data['text']
+    name = data['name']
+    message = {
+        'text': text,
+        'name': name,
+        'time': time.time()
+    }
+    db.append(message)
+
+    if text == '\help':
+        bot = {'text': 'Для работы с мессенджером запустите server.py, sender.py, receiver.py.\n\
+Проверьте, что сервер находится в запущенном состоянии до запуска остальных программ.\n\
+Напишите через слеш интересующие вас разделы: status, messages, send, index\n',
+                        'name': 'bot', 'time': time.time()
+               }
+        db.append(bot)
     return {'ok': True}
 
-@app.get("/messages")
-def get_messages(after: float):
-    db_after = [message for message in db if message['time'] > after]
+@app.route("/messages")
+def get_messages():
+    try:
+        after = float(flask.request.args['after'])
+    except:
+        abort(400)
+    db_after = []
+    for message in db:
+        if message['time'] > after:
+            db_after.append(message)
     return {'messages': db_after}
 
-@app.get("/status")
+@app.route("/status")
 def print_status():
     total_users = len(set(message['name'] for message in db))
     total_messages = len(db)
+    info1 = ['city: Москва', 'postalCode: 117198']
+    info2 = ['Number of users:', '----------------', total_users,'--------------------', 'Number of messages: ','--------------------', total_messages]
+    info = ['Name: Аиша', '----------', 'ADDRESS:', '--------', info1, 'INFO:', '-----', info2]
+    return info
 
-    status_info = {
-        "status": "Ok!",
-        "current_time": time.ctime(),
-        "total_users": total_users,
-        "total_messages": total_messages
-    }
-
-    return status_info
-
-@app.get('/index')
-def lionel():
+@app.route('/index')
+def lionel(): 
     return flask.render_template('index.html')
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-
+print(print_status())
+app.run()
